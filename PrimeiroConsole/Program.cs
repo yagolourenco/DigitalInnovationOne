2d56fb6df9a041052a0705744bc6cb25122ce023
Yago Pablo Lourenço Vaz <yago.plv@hotmail.com>
2021-02-08 15:38:21 -0300

    primeiro commit

---
+++ PrimeiroConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
